2d26bb0ed4c20d07eee4e04aab723590c75f8973
Bevan Arps <bevan.arps@microsoft.com>
2023-09-11 12:03:02 +1200

    Update generated code for compute

--- v2/api/compute/customizations/disk_extension_types_gen.go
+++ v2/api/compute/customizations/disk_extension_types_gen.go
@@ -5,9 +5,9 @@ package customizations
 
 import (
 	v20200930 "github.com/Azure/azure-service-operator/v2/api/compute/v1api20200930"
-	v20200930s "github.com/Azure/azure-service-operator/v2/api/compute/v1api20200930storage"
+	v20200930s "github.com/Azure/azure-service-operator/v2/api/compute/v1api20200930/storage"
 	v1beta20200930 "github.com/Azure/azure-service-operator/v2/api/compute/v1beta20200930"
-	v1beta20200930s "github.com/Azure/azure-service-operator/v2/api/compute/v1beta20200930storage"
+	v1beta20200930s "github.com/Azure/azure-service-operator/v2/api/compute/v1beta20200930/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -17,8 +17,8 @@ type DiskExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *DiskExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
-		&v20200930.Disk{},
-		&v20200930s.Disk{},
 		&v1beta20200930.Disk{},
-		&v1beta20200930s.Disk{}}
+		&v1beta20200930s.Disk{},
+		&v20200930.Disk{},
+		&v20200930s.Disk{}}
 }
