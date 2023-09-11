738ec1772b6d55ceee438a91932a7b432b4fec24
Bevan Arps <bevan.arps@microsoft.com>
2023-09-11 12:03:13 +1200

    Update generated code for eventgrid

--- v2/api/eventgrid/customizations/domain_extension_types_gen.go
+++ v2/api/eventgrid/customizations/domain_extension_types_gen.go
@@ -5,9 +5,9 @@ package customizations
 
 import (
 	v20200601 "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601"
-	v20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601storage"
+	v20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601/storage"
 	v1beta20200601 "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601"
-	v1beta20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601storage"
+	v1beta20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -17,8 +17,8 @@ type DomainExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *DomainExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
-		&v20200601.Domain{},
-		&v20200601s.Domain{},
 		&v1beta20200601.Domain{},
-		&v1beta20200601s.Domain{}}
+		&v1beta20200601s.Domain{},
+		&v20200601.Domain{},
+		&v20200601s.Domain{}}
 }
--- v2/api/eventgrid/customizations/domains_topic_extension_types_gen.go
+++ v2/api/eventgrid/customizations/domains_topic_extension_types_gen.go
@@ -5,9 +5,9 @@ package customizations
 
 import (
 	v20200601 "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601"
-	v20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601storage"
+	v20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1api20200601/storage"
 	v1beta20200601 "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601"
-	v1beta20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601storage"
+	v1beta20200601s "github.com/Azure/azure-service-operator/v2/api/eventgrid/v1beta20200601/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -17,8 +17,8 @@ type DomainsTopicExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *DomainsTopicExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
-		&v20200601.DomainsTopic{},
-		&v20200601s.DomainsTopic{},
 		&v1beta20200601.DomainsTopic{},
-		&v1beta20200601s.DomainsTopic{}}
+		&v1beta20200601s.DomainsTopic{},
+		&v20200601.DomainsTopic{},
+		&v20200601s.DomainsTopic{}}
 }
