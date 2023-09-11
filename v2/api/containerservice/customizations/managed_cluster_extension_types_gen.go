65db978867e057e8cae7d3ae0442b991beb1c2ca
Bevan Arps <bevan.arps@microsoft.com>
2023-09-11 12:03:05 +1200

    Update generated code for containerservice

--- v2/api/containerservice/customizations/managed_cluster_extension_types_gen.go
+++ v2/api/containerservice/customizations/managed_cluster_extension_types_gen.go
@@ -5,13 +5,13 @@ package customizations
 
 import (
 	v20210501 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501"
-	v20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501storage"
+	v20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501/storage"
 	v20230201 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201"
-	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201storage"
+	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201/storage"
 	v20230202p "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202preview"
-	v20230202ps "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202previewstorage"
+	v20230202ps "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202preview/storage"
 	v1beta20210501 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501"
-	v1beta20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501storage"
+	v1beta20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -21,12 +21,12 @@ type ManagedClusterExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *ManagedClusterExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
+		&v1beta20210501.ManagedCluster{},
+		&v1beta20210501s.ManagedCluster{},
 		&v20210501.ManagedCluster{},
 		&v20210501s.ManagedCluster{},
 		&v20230201.ManagedCluster{},
 		&v20230201s.ManagedCluster{},
 		&v20230202p.ManagedCluster{},
-		&v20230202ps.ManagedCluster{},
-		&v1beta20210501.ManagedCluster{},
-		&v1beta20210501s.ManagedCluster{}}
+		&v20230202ps.ManagedCluster{}}
 }
--- v2/api/containerservice/customizations/managed_clusters_agent_pool_extension_types_gen.go
+++ v2/api/containerservice/customizations/managed_clusters_agent_pool_extension_types_gen.go
@@ -5,13 +5,13 @@ package customizations
 
 import (
 	v20210501 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501"
-	v20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501storage"
+	v20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20210501/storage"
 	v20230201 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201"
-	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201storage"
+	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201/storage"
 	v20230202p "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202preview"
-	v20230202ps "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202previewstorage"
+	v20230202ps "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202preview/storage"
 	v1beta20210501 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501"
-	v1beta20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501storage"
+	v1beta20210501s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1beta20210501/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -21,12 +21,12 @@ type ManagedClustersAgentPoolExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *ManagedClustersAgentPoolExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
+		&v1beta20210501.ManagedClustersAgentPool{},
+		&v1beta20210501s.ManagedClustersAgentPool{},
 		&v20210501.ManagedClustersAgentPool{},
 		&v20210501s.ManagedClustersAgentPool{},
 		&v20230201.ManagedClustersAgentPool{},
 		&v20230201s.ManagedClustersAgentPool{},
 		&v20230202p.ManagedClustersAgentPool{},
-		&v20230202ps.ManagedClustersAgentPool{},
-		&v1beta20210501.ManagedClustersAgentPool{},
-		&v1beta20210501s.ManagedClustersAgentPool{}}
+		&v20230202ps.ManagedClustersAgentPool{}}
 }
